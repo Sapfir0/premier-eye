61ac3aa0723c2aa8cc7fa5b5501f14b89952c760
Sapfir0 <sapfir999999@yandex.ru>
2019-07-10 13:25:33 +0400

    плохая алгоритмика

--- neural_network/maskCNN.py
+++ neural_network/maskCNN.py
@@ -174,14 +174,24 @@ def createHeatMap(image, filename):
     cv2.imwrite(f"{cfg.OUTPUT_DIR_MASKCNN}/{filename}", im_color )
 
 
-def getConcetration(highlightedRect, startTime, endTime): # координаты прямоугольника, в котором начинаем искать объекты
+def getConcetration(highlightedRect, objectsRect, startTime, endTime): # координаты прямоугольника, в котором начинаем искать объекты
+    def isCompletelyInside(bigRect, minRect)
+        y1, x1, y2, x2 = bigRect
+        minX = x1; minY = y2 # вроде верно
+        maxX = x2; maxY = y1
+
+        y1, x1, y2, x2 = minRect
+        
+        a = y1 >= miny and y1 <= maxY
+        b = x1 >= minX and x1 <= maxX
+        c = y2 >= miny and y2 <= maxY
+        d = x2 >= minX and x2 <= maxX
+        if (a and b and c and d):
+            return True # объект полностью внутри большого прямоугольника
+        return False
 
 
-
-    return foundedObjects # массив координат всех объектов в кадре
-
-
-    def isLittleInBig(bigRect, minRect):
+    def isPartiallyInside(bigRect, minRect):
         bigLUy, bigLUx, bigRDy, bigRDx = bigRect
         minLUy, minLUx, minRDy, minRDx = minRect
         if (bigLUy < minLUy):
@@ -193,4 +203,11 @@ def getConcetration(highlightedRect, startTime, endTime): # координаты
         if (bigRDx < minRDx):
             minRDx = bigRDx
         return False
+
+    for obj in objectsRect:
+        if ( isCompletelyInside(highlightedRect, obj) )
+            print("Объект попадает в кадр")
+
+
+    return foundedObjects # массив координат всех объектов в кадре
 
