89bfe3e7e24ef22d9095d7bc3f296ed7b60dfbc9
Sapfir0 <sapfir999999@yandex.ru>
2019-07-16 21:13:59 +0400

    resources

---
+++ resources/bash/rows_counter.sh
@@ -0,0 +1,1 @@
+find ./ -not -path "./data/*" -not -path "./output/*" -not -path "./resourses/images/*" -not -path "./README.md" -not -path "./LICENSE" -not -path "./.git/*" -not -path "./.vscode/*" -type f | xargs wc -l 
