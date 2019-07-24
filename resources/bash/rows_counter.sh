4fba6797c2c7ac1d8acfeaee17a57f6a7442112a
Alex Yurev <37454791+Sapfir0@users.noreply.github.com>
2019-07-24 14:03:02 +0400

    flask (#10)
    * Разделение

--- resources/bash/rows_counter.sh
+++
@@ -1,1 +0,0 @@
-find ./ -not -path "./data/*" -not -path "./__pycache__/*" -not -path "./output/*" -not -path "./resourses/images/*" -not -path "./README.md" -not -path "./LICENSE" -not -path "./.git/*" -not -path "./.vscode/*" -type f | xargs wc -l 
