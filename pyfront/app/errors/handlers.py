24d2d2f944d7d649ed9fafcd87786ae5a5fef0ff
Sapfir0 <sapfir999999@yandex.ru>
2019-07-26 00:43:08 +0400

    Упрощения и абстракции

--- pyfront/app/errors/handlers.py
+++ pyfront/app/errors/handlers.py
@@ -1,13 +1,12 @@
 from flask import render_template
-from app import app
-#from app import db
-
-@app.error_handler(404)
-def notFoundError(error):
-    return render_template('404.html'), 404
+from app.errors import bp
 
 
-@app.errorhandler(500)
+@bp.app_errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+
+@bp.app_errorhandler(500)
 def internal_error(error):
-    #db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
