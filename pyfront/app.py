c5b2d3411565b0f5c0cc0ab49900619473073120
Sapfir0 <sapfir999999@yandex.ru>
2019-09-19 22:59:18 +0400

    Доработка до heroku

--- pyfront/app.py
+++ pyfront/app.py
@@ -1,7 +1,7 @@
 from application import createApp
-import Config as cfg
+from config import Config as cfg
 
 app = createApp()
 
 if __name__ == "__main__":  # хм это же мейн
-    app.run(cfg.FLASK_RUN_HOST , debug=True)
+    app.run(cfg.FLASK_RUN_HOST, debug=True)
