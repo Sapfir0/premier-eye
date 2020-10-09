97203095628976aa6b4360696ef4d9cafa85840c
Alexander Yuriev <sapfir999999@yandex.ru>
2020-10-09 23:18:14 +0400

    Попробовал немного изменить всякое разное

--- Premier-eye.API/controllers/gallery/imageInfo.py
+++ Premier-eye.API/controllers/gallery/imageInfo.py
@@ -1,13 +1,19 @@
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from controllers.gallery import routes
 import database.dbAPI as db
 from flask_restplus import Namespace, Resource, fields
-from services.jsonWorking import addObjectToSession
+from database.models.Cars import Cars
+from database.models.Persons import Persons
+from database.models.Objects_ import Objects_
+from database.models.Images import Images, session
+from database.models.Coordinates import Coordinates
 
 
-def initImageInfo(api):
-    model = api.model('ImageInfo', {
-
+def initImageInfo(api: Namespace):
+    model = api.model('ObjectInfo', {
+        'id': fields.Integer,
+        'type': fields.String,
+        'scores': fields.Float,
     })
 
     @api.route(routes['getImageInfo'])
@@ -24,7 +30,29 @@ def initImageInfo(api):
 
             return jsonify(dict(imageInfo))
 
-        @api.marshal_with(model)
+        @api.expect(model)
         def post(self, filename):
-            addObjectToSession(dictObjects)
+            objects = request.form
+            countOfImagesInDB = session.query(Images).count() + 1  # imageId
+            # +1 т.к. у нас возвращается текущее колво строк, а мы будем инсертить еще одну
+            countOfObjectsInDB = session.query(Objects_).count() + 1  # objectId TODO
+            for detected in objects:
+                coordinates = Coordinates(detected['coordinates'])
+                Object = Objects_(scores=detected['scores'], typesOfObject=detected['type'],
+                                  imageId=countOfImagesInDB, coordinatesId=countOfObjectsInDB)
+
+                if detected['type'] == 'car':  # TODO кал
+                    car = Cars(carNumber=detected['licenseNumber'], objectId=countOfObjectsInDB)
+                    session.add(car)
+                elif detected['type'] == 'person':
+                    person = Persons(objectId=countOfObjectsInDB)
+                    session.add(person)
+                else:
+                    make_response({"error": "Undefined object"}, 400)
+
+                session.add(coordinates)
+                session.add(Object)
+
+                session.commit()
+                session.flush()
 
