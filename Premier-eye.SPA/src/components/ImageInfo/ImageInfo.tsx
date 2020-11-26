4acf8e0e6d828e13ae5a21e8e179b4061d2590e5
Alexander Yuryev <alexander.yuryev@singularis.local>
2020-11-26 12:53:53 +0400

    Исправил добавление элементов

--- Premier-eye.SPA/src/components/ImageInfo/ImageInfo.tsx
+++ Premier-eye.SPA/src/components/ImageInfo/ImageInfo.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import {Collapse, Divider, List, ListItem, ListItemText} from "@material-ui/core";
+import { Collapse, Divider, List, ListItem, ListItemText } from "@material-ui/core";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import TitledCameraNumber from "../Atomics/TitledCameraNumber";
-import {definitions} from "../../typings/Dto";
-import {getSettings, Settings} from "./SettingsHelper";
-import {detectionsImages} from "./ObjectsImages";
-import {WarningIfBigDiffBetweenDates} from "../Atomics/Warning/Warning"
+import { definitions } from "../../typings/Dto";
+import { getSettings, Settings } from "./SettingsHelper";
+import { detectionsImages } from "./ObjectsImages";
+import { WarningIfBigDiffBetweenDates } from "../Atomics/Warning/Warning"
 
 
-const  styles = {
+const styles = {
     root: {
         marginRight: "15px"
     },
@@ -33,14 +33,14 @@ interface IState {
 class ImageInfo extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props)
-        this.state = {settings: getSettings(10)}; // учет максимум 10 объектов на кадре
+        this.state = { settings: getSettings(10) }; // учет максимум 10 объектов на кадре
     }
 
     handleClick = (id: number) => {
         this.setState(state => ({
             ...state,
             settings: state.settings.map((item: Settings) =>
-                item.id === id ? {...item, open: !item.open} : item
+                item.id === id ? { ...item, open: !item.open } : item
             )
         }));
     };
@@ -50,9 +50,8 @@ class ImageInfo extends React.Component<IProps, IState> {
 
         if (!this.state.settings) {
             this.setState({
-                    settings: getSettings(data.length)
-                }
-            )
+                settings: getSettings(data.length)
+            })
         }
 
         for (let i = 0; i < data.length; i++) { // фиксим объект, нам было бы удобно, чтобы у него был порядковый номер
@@ -76,20 +75,18 @@ class ImageInfo extends React.Component<IProps, IState> {
             return <React.Fragment key={each.id}>
                 <ListItem button onClick={() => this.handleClick(each.id)}>
                     <ListItemIcon>{detectionsImages[each.type].icon} </ListItemIcon>
-                    <ListItemText inset primary={detectionsImages[each.type].title}/>
+                    <ListItemText inset primary={detectionsImages[each.type].title} />
                 </ListItem>
-                <Divider/>
+                <Divider />
                 {collapse}
             </React.Fragment>
         }
 
-        const red = (pV: Array<JSX.Element>, cV: definitions['ObjectInfo']) => {
-            pV.push(parse(cV));
-            return pV;
-        }
 
         objects = <List component="nav">
-            {data.reduce(red, [])}
+            {
+                data.map((el: definitions['ObjectInfo']) => parse(el))
+            }
         </List>
         return objects
 
@@ -98,7 +95,7 @@ class ImageInfo extends React.Component<IProps, IState> {
 
     render() {
         const myData = this.props.info
-        const {classes} = this.props;
+        const { classes } = this.props;
 
         let objects: JSX.Element | undefined;
         if (myData.objects) {
@@ -115,7 +112,7 @@ class ImageInfo extends React.Component<IProps, IState> {
             <div className={classes.root}>
                 <List component="nav" aria-label="main mailbox folders">
                     <ListItem>
-                        <TitledCameraNumber cameraId={myData.numberOfCam}/>
+                        <TitledCameraNumber cameraId={myData.numberOfCam} />
                     </ListItem>
                     <ListItem> {myData.filename} </ListItem>
                     <ListItem> {myData.fixationDatetime.toString()} {warningDateDiff}</ListItem>
