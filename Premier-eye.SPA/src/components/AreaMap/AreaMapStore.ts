4f2ae53de02f8e7360b52ffdc9eaec57680e7546
Alexander Yuryev <alexander.yuryev@singularis.local>
2020-12-10 15:48:14 +0400

    Добавил кнопку загрузки файла

--- Premier-eye.SPA/src/components/AreaMap/AreaMapStore.ts
+++ Premier-eye.SPA/src/components/AreaMap/AreaMapStore.ts
@@ -1,6 +1,13 @@
-import { observable } from "mobx";
+import { injectable } from "inversify";
+import { action, observable } from "mobx";
 
 
+@injectable()
 export class AreaMapStore {
     @observable mapUrl: string | null = null
+
+    @action
+    public addNewCard = () => {
+
+    }
 }
---
+++ Premier-eye.SPA/src/components/Atomics/UploadButton/UploadButton.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import Button from '@material-ui/core/Button';
+import "./UploadButton.pcss"
+
+
+export const UploadButton = (props: any) => { // buttonProps
+    return <>
+        <input
+            accept="image/*"
+            id="contained-button-file"
+            className="uploadButton"
+            type="file"
+        />
+        <label htmlFor="contained-button-file">
+            <Button {...props} variant="contained" color="primary" component="span">
+                {props.children}
+            </Button>
+        </label>
+
+    </>
+}
