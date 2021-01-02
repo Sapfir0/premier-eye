276e193ed6a63636914761ec62dc791cdb229773
Sapfir0 <sapfir999999@yandex.ru>
2021-01-02 21:54:34 +0300

    Попробовал поправить

--- Premier-eye.DS/services/apiWorkers/apiHelper.py
+++ Premier-eye.DS/services/apiWorkers/apiHelper.py
@@ -1,7 +1,6 @@
 import requests
 from requests import Response, Request
 from typing import Optional
-from pymonad import either
 from config.settings import Settings as config
 
 
@@ -9,12 +8,12 @@ class ApiHelper:
     def request(self, method, url, **reqKArgs):
         req: Response = requests.request(method, url, **reqKArgs)
         if req.status_code != 200:
-            print(req.json())
+            print(req.text())
             if config.strongRequestChecking:
                 raise Exception(f"[strongRequestChecking] Завершаю работу, запрос вернулся с кодом {req.status_code}.")
-
-            return either.Left(req.json())
+            
+            return req.json()
         
-        return either.Right(req.json())
+        return req.json()
 
 
--- Premier-eye.DS/services/apiWorkers/apiInteractionService.py
+++ Premier-eye.DS/services/apiWorkers/apiInteractionService.py
@@ -5,15 +5,15 @@ import os
 from config.settings import config
 from Models import Image
 import json
-from pymonad import either
+
 
 class ApiInteractionService(BaseInteractionService):
 
-    def postImageInfo(self, imagePath: str, imageInfo) -> either:
+    def postImageInfo(self, imagePath: str, imageInfo):
         jsonInfo = {"objects": imageInfo.json()}
         return self.post(galleryRoutes['postInfo'](os.path.basename(imagePath)), json=jsonInfo)
 
-    def uploadImage(self, imagePath: str) -> either:
+    def uploadImage(self, imagePath: str):
         if platform == "linux" or platform == "linux2":
             filename = os.path.split(imagePath)[1]  # TODO Only for linux!!
         else:
