04e0af213ea76ed6ebff76df88e5643389077cac
Sapfir0 <sapfir999999@yandex.ru>
2021-01-05 15:53:46 +0300

    Правки

--- Premier-eye.SPA/src/components/Atomics/ProgressBar/DesktopProgressBar.tsx
+++ Premier-eye.SPA/src/components/Atomics/ProgressBar/DesktopProgressBar.tsx
@@ -2,12 +2,15 @@ import React, { useEffect, useRef, useState } from 'react';
 import { range } from '../../../services/utils';
 import { IStepper } from '../../ImageViewer/ImageView/Steppers/IStepper';
 import './DesktopProgressBar.pcss';
+import {getDatetimeFromFilename} from "../../../services/DateFormatter";
 
 const toPixels = (num: number) => `${num}px`;
 
 export const DesktopProgressBar = (props: IStepper) => {
     const [playerWidth, setPlayerWidth] = useState(600);
     const frameLength = playerWidth / props.images.length;
+
+    const [frameTime, setFrameTime] = useState('');
 
     const ref = useRef<HTMLDivElement>(null);
     useEffect(() => {
@@ -18,20 +21,30 @@ export const DesktopProgressBar = (props: IStepper) => {
         props.changeCurrentStep(frameIndex);
     };
 
-    const onMouseEnter = (frameIndex: number) => (e: React.MouseEvent<HTMLDivElement>) => {};
+    const onMouseEnter = (frameIndex: number) => (e: React.MouseEvent<HTMLDivElement>) => {
+
+        const src = props.images[frameIndex].src
+        const datetime = getDatetimeFromFilename(src);
+        if (datetime !== null) {
+            const time = `${datetime.getHours()}:${datetime.getMinutes()}:${datetime?.getSeconds()}`;
+            setFrameTime(time);
+        }
+
+    };
 
     return (
         <>
             <div ref={ref} className="outside">
+
                 <div className="progress" style={{ width: toPixels(props.currentStep * frameLength) }} />
                 <div className="pointsContainer">
-                    {range(props.images.length).map((empty, i) => (
+                    {props.images.map((empty, i) => (
                         <div
-                            key={i}
+                            key={empty.id}
                             className="inside"
                             onMouseEnter={onMouseEnter(i)}
                             style={{ width: frameLength }}
-                            onClick={moving(i)}
+                            onClick={moving(parseInt(empty.id))}
                         />
                     ))}
                 </div>
---
+++ Premier-eye.SPA/src/services/DateFormatter.ts
@@ -0,0 +1,10 @@
+import {timeParse} from "d3-time-format";
+import {dateFileFormat} from "../config/constants";
+
+
+export const getDatetimeFromFilename = (filename: string) => {
+    // js implementation of some functions in Premier-eye.Common
+    const dateTime = filename.split('_')[1].split('.')[0];
+    const parseTime = timeParse(dateFileFormat);
+    return parseTime(dateTime);
+};
