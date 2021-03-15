7aa6194927a766c159c6a323774c3cf75d535687
Sapfir0 <sapfir999999@yandex.ru>
2021-03-15 19:52:41 +0300

    Added area map css

--- Premier-eye.SPA/src/components/AreaMap/AreaMap.tsx
+++ Premier-eye.SPA/src/components/AreaMap/AreaMap.tsx
@@ -25,6 +25,8 @@ const getDistance = (lat1: number, lon1: number, lat2: number, lon2: number) => 
     return d;
 };
 
+// В данном случае всегда первые 2 координаты - это координаты маленького основания, а последние 2 - большого
+
 const camera07 = [
     { lat: 48.77073964423231, lng: 44.585335850715644 },
     { lat: 48.770718430949216, lng: 44.58525538444519 },
@@ -69,12 +71,31 @@ const realCamera01 = { lat: 48.77081389065253, lon: 44.58502471446992 };
 const realCamera03 = [48.77005727913369, 44.58472967147827];
 const realCamera07 = [48.770782070771595, 44.5852392911911];
 
+const getTrapeziumHeight = () => {
+    const smallBase = getDistance(camera03[0].lat, camera03[0].lng, camera03[1].lat, camera03[1].lng);
+    const bigBase = getDistance(camera03[2].lat, camera03[2].lng, camera03[3].lat, camera03[3].lng);
+    console.log(smallBase, bigBase);
+    console.log(bigBase / smallBase);
+    const c = getDistance(camera03[1].lat, camera03[1].lng, camera03[2].lat, camera03[2].lng); // а вот тут можно ошибиться и получить диагонали
+    const d = getDistance(camera03[0].lat, camera03[0].lng, camera03[3].lat, camera03[3].lng);
+
+    const dividend = (bigBase - smallBase) ** 2 + c ** 2 - d ** 2;
+    const divider = 2 * (bigBase - smallBase);
+    const inBigFract = (dividend / divider) ** 2;
+
+    const res = c ** 2 - inBigFract;
+    const h = Math.sqrt(res);
+    return h;
+};
+
 export const AreaMap = observer(() => {
-    for (const firstGeo of camera03) {
-        for (const secondGeo of camera03) {
-            console.log(getDistance(firstGeo.lat, firstGeo.lng, secondGeo.lat, secondGeo.lng));
-        }
-    }
+    // for (const firstGeo of camera03) {
+    //     for (const secondGeo of camera03) {
+    //         console.log(getDistance(firstGeo.lat, firstGeo.lng, secondGeo.lat, secondGeo.lng));
+    //     }
+    // }
+    console.log(getTrapeziumHeight());
+
     return (
         <>
             Карта
@@ -88,7 +109,6 @@ export const AreaMap = observer(() => {
 
                 <Circle pathOptions={{ color: 'orange', fillColor: 'orange' }} center={realCamera02} radius={1} />
                 <Polygon pathOptions={{ color: 'orange' }} positions={camera02} />
-
                 <Circle pathOptions={{ color: 'red', fillColor: 'red' }} center={realCamera01} radius={1} />
                 <Polygon pathOptions={{ color: 'red' }} positions={camera01} />
                 <LocationMarker />
